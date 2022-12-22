721603ae5c659a09713995df1780d22935b81e53
Brandon Haupt <haupt.brandon@gmail.com>
2022-12-21 20:03:17 -0600

    Update 1.4
    - setup the movie display component to show no movie loaded or movie is loaded

--- react_movie/src/components/MovieDisplay.js
+++ react_movie/src/components/MovieDisplay.js
@@ -1,7 +1,32 @@
 
-export default function MovieDisplay({movie}) {
-    return(<div class="display">
-        <h1>{movie.Title}</h1>
-        <img src={movie.Poster} alt={movie.Title}/>
-    </div>)
+export default function MovieDisplay( {movie} ) {
+    
+    //! This will only run if we have the data for the movie
+    const loaded = () => {
+        return(
+            <div class="display">
+                <div className="">
+                    <h1>{movie.Title}</h1>
+                    <h2>{movie.Year}</h2>
+                    <h3>{movie.Rated}</h3>
+                </div>
+               
+               <div>
+                <img src={movie.Poster} alt={movie.Title}/>
+               </div>
+    
+                <p>Movie Rating: {movie.imdbRating}/10</p>
+                <p>Plot - {movie.Plot}</p>
+            </div>
+        )
+    }
+   
+    //! this runs only if there is no movie data
+    const loading = () => {
+        return <h1>No Movie to Display</h1>
+    }
+
+    //! This says if loaded it will run loaded but if false run loading function
+    return movie ? loaded() : loading()
+    
 }
