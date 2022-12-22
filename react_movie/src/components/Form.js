15073440fa0accc71f2ca7c7f262cecb332fc19d
Brandon Haupt <haupt.brandon@gmail.com>
2022-12-21 18:21:05 -0600

    Update 1.1
    -setting up react to allow for API Calls

---
+++ react_movie/src/components/Form.js
@@ -0,0 +1,12 @@
+
+
+function Form(props){
+    return(<div>
+        <form>
+            <input />
+            <input type="submit" value="submit" />
+        </form>
+    </div>)
+}
+
+export default Form
---
+++ react_movie/src/components/MovieDisplay.js
@@ -0,0 +1,9 @@
+
+function MovieDisplay(props) {
+    return(<div class="display">
+        <h1>Hello Display</h1>
+    
+    </div>)
+}
+
+export default MovieDisplay
